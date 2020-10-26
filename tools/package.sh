c6c7ebc200ac00de32dddcb759429a26d7889f5e
Samuel Sarette <samuelsarette@linux.com>
2020-10-26 18:55:44 -0400

    Fix the ability to make a nwjs app

--- tools/package.sh
+++ tools/package.sh
@@ -2,6 +2,8 @@
 
 VERSION=$(grep "\"version\"" ../package.json | cut -d':' -f 2,3 | cut -d'"' -f2)
 
+echo "Building version $VERSION"
+
 mkdir -p downloads
 wget -q -i nwjs-downloads.txt -nc -c -P downloads
 
@@ -18,6 +20,7 @@ cp -r ../LICENSE ../dist/package-contents/
 cp -r ../index.html ../dist/package-contents/
 cp -r ../main.css ../dist/package-contents/
 cp -r ../manifest.webapp ../dist/package-contents/
+cp -r ../package.nwjs.json ../dist/package-contents/package.json
 cp -r ../js ../dist/package-contents/
 cp -r ../json ../dist/package-contents/
 cp -r ../media ../dist/package-contents/
@@ -28,7 +31,7 @@ tar -zxvf downloads/nwjs-*-linux-x64.tar.gz -C ../dist/
 mv ../dist/nwjs-*-linux-x64 ../dist/exchanges-game
 mv ../dist/exchanges-game/nw ../dist/exchanges-game/exchanges-game
 chmod +x ../dist/exchanges-game/exchanges-game
-cd ../dist/package-contents/;
+cd ../dist/package-contents/
 zip -r ../exchanges-game/package.nw *
 cd ../exchanges-game/
 zip -r ../../output/exchanges-game.zip  *
